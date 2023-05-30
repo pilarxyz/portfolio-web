22c29368ef4f3d223bf020b2869f612dc5a0b85e
pilarxyz <pilar@student.uns.ac.id>
2023-05-30 11:55:37 +0700

    justify content

--- pages/about.js
+++ pages/about.js
@@ -2,55 +2,116 @@ import Layout from "../layout/layout";
 import { motion } from "framer-motion";
 
 export default function About() {
-    return (
-        <Layout title="About me">
-            <div className="flex flex-grow items-center">
-                <div className="w-full">
-                    <div className="container flex mx-auto justify-center">
-                        <motion.div initial={{ opacity: 0 }} animate={{ opacity: [0, 1] }} transition={{ duration: 0.5 }}>
-                            <h1 className="text-center md:text-6xl text-3xl font-bold">
-                                About <span className="text-gray-500">me</span>
-                            </h1>
-                            <div className="container lg:w-3/5 w-11/12 mx-auto mt-4">
-                                <div className="text-left md:text-justify space-y-6 text-lg font-medium text-gray-200">
-                                    <p>
-                                    Hello there! I'm Pilar Rangga Saputra, a budding Computer Science whiz studying at Sebelas Maret University and making strides as a Junior Web Developer. Armed with creativity and tech-savviness, I delight in crafting interactive websites and staying on top of the latest industry trends. When I'm not coding or studying, you can find me immersed in thrilling video games or wandering off to explore unseen paths. Eagerly embracing opportunities to learn and grow, I'm ready to shape the digital world. Let's embark on this exciting journey together!
-                                    </p>
-                                    <div className="space-y-2">
-                                        <p>
-                                        Proficiently wielding the following languages:
-                                        </p>
-                                        <div className="grid grid-cols-2 grid-rows-3 md:grid-cols-3 md:grid-rows-2 lg:grid-rows-1 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
-                                            <img alt="PHP" src="https://img.shields.io/badge/-PHP-18181b?style=for-the-badge&logo=php&logoColor=ffffff" />
-                                            <img alt="Python" src="https://img.shields.io/badge/-Python-18181b?style=for-the-badge&logo=python&logoColor=ffffff" />
-                                            <img alt="SQL" src="https://img.shields.io/badge/-SQL-18181b?style=for-the-badge&logo=postgresql&logoColor=ffffff" />
-                                            <img alt="Javascript" src="https://img.shields.io/badge/-Javascript-18181b?style=for-the-badge&logo=javascript&logoColor=ffffff" />
-                                            <img alt="HTML5" src="https://img.shields.io/badge/-HTML5-18181b?style=for-the-badge&logo=html5&logoColor=ffffff" />
-                                            <img alt="CSS3" src="https://img.shields.io/badge/-CSS3-18181b?style=for-the-badge&logo=css3&logoColor=ffffff" />
-                                        </div>
-                                        <p>
-                                        Proficiently maneuvering these frameworks and libraries:
-                                        </p>
-                                        <div className="grid grid-cols-2 grid-rows-3 md:grid-cols-3 md:grid-rows-2 lg:grid-rows-1 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
-                                            <img alt="Laravel" src="https://img.shields.io/badge/-Laravel-18181b?style=for-the-badge&logo=laravel&logoColor=ffffff" />
-                                            <img alt="CodeIgniter" src="https://img.shields.io/badge/-CodeIgniter-18181b?style=for-the-badge&logo=codeigniter&logoColor=ffffff" />
-                                            <img alt="Tailwind CSS" src="https://img.shields.io/badge/-Tailwind CSS-18181b?style=for-the-badge&logo=tailwindcss&logoColor=ffffff" />
-                                            <img alt="Bootstrap" src="https://img.shields.io/badge/-Bootstrap-18181b?style=for-the-badge&logo=bootstrap&logoColor=ffffff" />
-                                            <img alt="Flask" src="https://img.shields.io/badge/-Flask-18181b?style=for-the-badge&logo=flask&logoColor=ffffff" />
-                                            <img alt="React" src="https://img.shields.io/badge/-React-18181b?style=for-the-badge&logo=react&logoColor=ffffff" />
-                                            <img alt="Next.js" src="https://img.shields.io/badge/-Next.js-18181b?style=for-the-badge&logo=next.js&logoColor=ffffff" />
-                                        </div>
-                                        <p>
-                                        Unfolding my craft with these miscellaneous tools:
-                                        </p>
-                                        <div className="grid grid-cols-2 grid-rows-3 md:grid-cols-3 md:grid-rows-2 lg:grid-rows-1 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
-                                            <img alt="Git" src="https://img.shields.io/badge/-Git-18181b?style=for-the-badge&logo=git&logoColor=ffffff" />
-                                            {/* <img alt="DigitalOcean" src="https://img.shields.io/badge/-DigitalOcean-18181b?style=for-the-badge&logo=digitalocean&logoColor=ffffff" /> */}
-                                            <img alt="VSCode" src="https://img.shields.io/badge/-VSCode-18181b?style=for-the-badge&logo=visual-studio-code&logoColor=ffffff" />
-                                            <img alt="Figma" src="https://img.shields.io/badge/-Figma-18181b?style=for-the-badge&logo=figma&logoColor=ffffff" />
-                                            <img alt="Docker" src="https://img.shields.io/badge/-Docker-18181b?style=for-the-badge&logo=docker&logoColor=ffffff" />
-                                        </div>
-                                        {/* <p>
+  return (
+    <Layout title="About me">
+      <div className="flex flex-grow items-center">
+        <div className="w-full">
+          <div className="container flex mx-auto justify-center">
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: [0, 1] }}
+              transition={{ duration: 0.5 }}
+            >
+              <h1 className="text-center md:text-6xl text-3xl font-bold">
+                About <span className="text-gray-500">me</span>
+              </h1>
+              <div className="container lg:w-3/5 w-11/12 mx-auto mt-4">
+                <div className="text-justify space-y-6 text-lg font-medium text-gray-200">
+                  <p>
+                    Hello there! I'm
+                    <b>Pilar Rangga Saputra</b>, a budding Computer Science whiz
+                    studying at Sebelas Maret University and making strides as a
+                    Junior Web Developer. Armed with creativity and
+                    tech-savviness, I delight in crafting interactive websites
+                    and staying on top of the latest industry trends. When I'm
+                    not coding or studying, you can find me immersed in
+                    thrilling video games or wandering off to explore unseen
+                    paths. Eagerly embracing opportunities to learn and grow,
+                    I'm ready to shape the digital world. Let's embark on this
+                    exciting journey together!
+                  </p>
+                  <div className="space-y-2">
+                    <p>Proficiently wielding the following languages:</p>
+                    <div className="grid grid-cols-2 grid-rows-3 md:grid-cols-3 md:grid-rows-2 lg:grid-rows-1 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+                      <img
+                        alt="PHP"
+                        src="https://img.shields.io/badge/-PHP-18181b?style=for-the-badge&logo=php&logoColor=ffffff"
+                      />
+                      <img
+                        alt="Python"
+                        src="https://img.shields.io/badge/-Python-18181b?style=for-the-badge&logo=python&logoColor=ffffff"
+                      />
+                      <img
+                        alt="SQL"
+                        src="https://img.shields.io/badge/-SQL-18181b?style=for-the-badge&logo=postgresql&logoColor=ffffff"
+                      />
+                      <img
+                        alt="Javascript"
+                        src="https://img.shields.io/badge/-Javascript-18181b?style=for-the-badge&logo=javascript&logoColor=ffffff"
+                      />
+                      <img
+                        alt="HTML5"
+                        src="https://img.shields.io/badge/-HTML5-18181b?style=for-the-badge&logo=html5&logoColor=ffffff"
+                      />
+                      <img
+                        alt="CSS3"
+                        src="https://img.shields.io/badge/-CSS3-18181b?style=for-the-badge&logo=css3&logoColor=ffffff"
+                      />
+                    </div>
+                    <p>
+                      Proficiently maneuvering these frameworks and libraries:
+                    </p>
+                    <div className="grid grid-cols-2 grid-rows-3 md:grid-cols-3 md:grid-rows-2 lg:grid-rows-1 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+                      <img
+                        alt="Laravel"
+                        src="https://img.shields.io/badge/-Laravel-18181b?style=for-the-badge&logo=laravel&logoColor=ffffff"
+                      />
+                      <img
+                        alt="CodeIgniter"
+                        src="https://img.shields.io/badge/-CodeIgniter-18181b?style=for-the-badge&logo=codeigniter&logoColor=ffffff"
+                      />
+                      <img
+                        alt="Tailwind CSS"
+                        src="https://img.shields.io/badge/-Tailwind CSS-18181b?style=for-the-badge&logo=tailwindcss&logoColor=ffffff"
+                      />
+                      <img
+                        alt="Bootstrap"
+                        src="https://img.shields.io/badge/-Bootstrap-18181b?style=for-the-badge&logo=bootstrap&logoColor=ffffff"
+                      />
+                      <img
+                        alt="Flask"
+                        src="https://img.shields.io/badge/-Flask-18181b?style=for-the-badge&logo=flask&logoColor=ffffff"
+                      />
+                      <img
+                        alt="React"
+                        src="https://img.shields.io/badge/-React-18181b?style=for-the-badge&logo=react&logoColor=ffffff"
+                      />
+                      <img
+                        alt="Next.js"
+                        src="https://img.shields.io/badge/-Next.js-18181b?style=for-the-badge&logo=next.js&logoColor=ffffff"
+                      />
+                    </div>
+                    <p>Unfolding my craft with these miscellaneous tools:</p>
+                    <div className="grid grid-cols-2 grid-rows-3 md:grid-cols-3 md:grid-rows-2 lg:grid-rows-1 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+                      <img
+                        alt="Git"
+                        src="https://img.shields.io/badge/-Git-18181b?style=for-the-badge&logo=git&logoColor=ffffff"
+                      />
+                      {/* <img alt="DigitalOcean" src="https://img.shields.io/badge/-DigitalOcean-18181b?style=for-the-badge&logo=digitalocean&logoColor=ffffff" /> */}
+                      <img
+                        alt="VSCode"
+                        src="https://img.shields.io/badge/-VSCode-18181b?style=for-the-badge&logo=visual-studio-code&logoColor=ffffff"
+                      />
+                      <img
+                        alt="Figma"
+                        src="https://img.shields.io/badge/-Figma-18181b?style=for-the-badge&logo=figma&logoColor=ffffff"
+                      />
+                      <img
+                        alt="Docker"
+                        src="https://img.shields.io/badge/-Docker-18181b?style=for-the-badge&logo=docker&logoColor=ffffff"
+                      />
+                    </div>
+                    {/* <p>
                                         Embarking on an exciting journey to master these skills:
                                         </p>
                                         <div className="grid grid-cols-2 grid-rows-3 md:grid-cols-3 md:grid-rows-2 lg:grid-rows-1 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
@@ -58,13 +119,13 @@ export default function About() {
                                             <img alt="Rust" src="https://img.shields.io/badge/-Rust-18181b?style=for-the-badge&logo=rust&logoColor=ffffff" />
                                             <img alt="Docker" src="https://img.shields.io/badge/-Docker-18181b?style=for-the-badge&logo=docker&logoColor=ffffff" />
                                         </div> */}
-                                    </div>
-                                </div>
-                            </div>
-                        </motion.div>
-                    </div>
+                  </div>
                 </div>
-            </div>
-        </Layout>
-    )
+              </div>
+            </motion.div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 }
